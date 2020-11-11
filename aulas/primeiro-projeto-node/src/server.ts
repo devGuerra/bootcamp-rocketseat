449ea429d6268d283ee0bb268635c874ec7584ae
Roberto Guerra <roberto.guerra@livup.com.br>
2020-11-10 21:44:25 -0300

    start prettier

--- aulas/primeiro-projeto-node/src/server.ts
+++ aulas/primeiro-projeto-node/src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: "Hello GoStack" })
-})
+app.get('/', (req, res) => res.json({ message: 'Hello GoStack' }));
 
 app.listen(3333, () => {
-  console.log('🛴 Server online on port 3333')
-})
+  console.log('🛴 Server online on port 3333');
+});
