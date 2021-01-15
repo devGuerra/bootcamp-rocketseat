5228e3d6d9ad34500978c2bae0af12e17a10970c
Roberto Guerra <prosaka@hotmail.com>
2021-01-14 21:43:13 -0300

    start typeorm

---
+++ aulas/backend/src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { response, Router } from 'express';
+import { parseISO } from 'date-fns';
+import {getCustomRepository} from 'typeorm'
+
+import AppointmentsRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  const appointments = appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
