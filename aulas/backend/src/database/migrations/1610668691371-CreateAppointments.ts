2a042886636e3c0b35edc6a08726769933e9a549
Roberto Guerra <prosaka@hotmail.com>
2021-01-15 19:25:53 -0300

    Aula finish

--- aulas/backend/src/database/migrations/1610668691371-CreateAppointments.ts
+++ aulas/backend/src/database/migrations/1610668691371-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1610668691371 implements MigrationInterfa
               type: 'varchar',
               isPrimary: true,
               generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()'
             },
             {
               name: "provider",
