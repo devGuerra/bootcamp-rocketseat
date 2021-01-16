7e6259e16b3fc0bac4cc14852cd480511d15d4e4
Roberto Guerra <prosaka@hotmail.com>
2021-01-15 21:41:17 -0300

    adicionar validacao jwt

---
+++ aulas/backend/src/config/auth.ts
@@ -0,0 +1,8 @@
+import { de } from "date-fns/locale";
+
+export default {
+  jwt: {
+    secret: 'b6768864cd977b4411f5e5d3ecdad410',
+    expiresIn: '1d'
+  }
+}
---
+++ aulas/backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+
+import authConfig from '../config/auth'
+
+interface TokenPayload {
+  iat: number
+  exp: number
+  sub: string
+}
+
+export default function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    throw new Error('Token is missing.')
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret) as TokenPayload
+
+    const { sub } = decoded
+
+    req.user = {
+      id: sub
+    }
+
+    return next()
+
+  } catch {
+    throw new Error('Invalid token')
+  }
+}
