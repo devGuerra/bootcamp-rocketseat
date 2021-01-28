a0f09f00fb0bba7aaae67af7960c27680acbbf8f
Roberto Guerra <prosaka@hotmail.com>
2021-01-28 18:57:12 -0300

    tratando erros

--- aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronRight } from 'react-icons/fi'
 import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 
 interface Repository {
   full_name: string
@@ -17,17 +17,28 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
   const [repositories, setRepositories] = useState<Repository[]>([])
 
   const handleAddRepository = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.')
+      return
+    }
 
-    const response = await api.get<Repository>(`repos/${newRepo}`)
 
-    const repository = response.data
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
 
-    setRepositories([...repositories, repository])
-    setNewRepo('')
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+      setInputError('')
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório.')
+    }
   }
 
   return (
@@ -35,9 +46,7 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios Github</Title>
 
-
-      <Form onSubmit={handleAddRepository}>
-
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -45,6 +54,10 @@ const Dashboard: React.FC = () => {
         />
         <button type='submit'>Pesquisar</button>
       </Form>
+
+      {
+        inputError && <Error>{inputError}</Error>
+      }
 
       <Repositories>
         {
