688f0c6da6984998d0c28e1e95e6dcf3c8bf1603
Roberto Guerra <prosaka@hotmail.com>
2021-01-28 18:43:31 -0300

    buscando repositorios

--- aulas/05-primeiro-projeto-react/src/routes/index.tsx
+++ aulas/05-primeiro-projeto-react/src/routes/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { Switch, Route } from 'react-router-dom'
 
-import Dashboard from './pages/Dashboard'
-import Repository from './pages/Repository'
+import Dashboard from '../pages/Dashboard'
+import Repository from '../pages/Repository'
 
 const Routes: React.FC = () => (
     <Switch>
