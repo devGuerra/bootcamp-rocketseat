3d3d22b195a50b0fa852c87ef1396c879c18c807
Roberto Guerra <prosaka@hotmail.com>
2021-01-28 20:42:44 -0300

    Estilizando detalhe

--- aulas/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ aulas/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+import { Header, RepositoryInfo, Issues } from './styles'
 
 interface RepositoryParams {
   repository: string
@@ -9,9 +13,52 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>()
 
   return (
-    <div>
-      <h1>Repository: {params.repository}</h1>
-    </div>
+    <>
+      <Header>
+        <img src={logoImg} alt="github explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/6579736?s=460&u=64eb8fc12ef135fdbccaace93551f58b6d9ffe31&v=4"
+            alt="Roberto Guerra"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`/repositories`}>
+          <div>
+            <strong>asd</strong>
+            <p>asdasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 }
 
