41f476a616d72cc6845cce9abb4def3acc475691
Roberto Guerra <prosaka@hotmail.com>
2021-01-28 19:19:39 -0300

    navegando entre rotas

--- aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ aulas/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import { FiChevronRight } from 'react-icons/fi'
 
 import api from '../../services/api'
@@ -73,14 +74,14 @@ const Dashboard: React.FC = () => {
       <Repositories>
         {
           repositories.map(repository => (
-            <a key={repository.full_name} href="">
+            <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
               <img src={repository.owner.avatar_url} alt={repository.owner.login} />
               <div>
                 <strong>{repository.full_name}</strong>
                 <p>{repository.description}</p>
               </div>
               <FiChevronRight size={20} />
-            </a>
+            </Link>
           ))
         }
       </Repositories>
