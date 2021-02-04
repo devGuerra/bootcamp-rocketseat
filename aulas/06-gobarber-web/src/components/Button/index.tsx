3d7b202b7ac0f7646e2809df066508acd9d1bf1e
Roberto Guerra <prosaka@hotmail.com>
2021-02-04 13:50:38 -0300

    mantendo usuario no storage

--- aulas/06-gobarber-web/src/components/Button/index.tsx
+++ aulas/06-gobarber-web/src/components/Button/index.tsx
@@ -1,16 +1,15 @@
-import React, { ButtonHTMLAttributes, HtmlHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
     <Container type="button" {...props}>
       {children}
     </Container>
-  )
-}
-
+  );
+};
 
 export default Button;
