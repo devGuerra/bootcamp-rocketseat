130dfcba79760d323cf9a1857fe16aeabee019a6
Roberto Guerra <prosaka@hotmail.com>
2021-02-04 14:00:20 -0300

    Logout da aplicação

--- aulas/06-gobarber-web/src/pages/SignIn/index.tsx
+++ aulas/06-gobarber-web/src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
