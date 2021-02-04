b7d0fcfcf221292a27c1b1fe6cbc5e3b69b71408
Roberto Guerra <prosaka@hotmail.com>
2021-02-03 22:26:58 -0300

    Usabilidade do input

--- aulas/06-gobarber-web/src/components/Input/index.tsx
+++ aulas/06-gobarber-web/src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons/lib/cjs';
 import { useField } from '@unform/core';
 
@@ -10,7 +16,9 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, Icon, ...rest }) => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
   useEffect(() => {
@@ -21,10 +29,30 @@ const Input: React.FC<InputProps> = ({ name, Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
 
+  /**
+   * Sempre que o componente sofre uma alteração ele renderiza todas as funções
+   * de dentro dele, usando o @useCallBack ele não re-cria a função
+   */
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
   return (
-    <Container>
+    <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon />}
-      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
     </Container>
   );
 };
