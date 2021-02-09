636f13efb3bd33423565786073b09a92372875ec
Roberto Guerra <prosaka@hotmail.com>
2021-02-08 22:50:29 -0300

    Tela de cadastro

--- aulas/07-gobarber-mobile/src/pages/SignUp/index.tsx
+++ aulas/07-gobarber-mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImage from '../../assets/logo.png';
+
+import { Container, Title, BackToSignin, BackToSigninText } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImage} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="user" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('click');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      {/* TODO: Remover esse botao ao mostrar o teclado */}
+      <BackToSignin
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSigninText>Voltar para logon</BackToSigninText>
+      </BackToSignin>
+    </>
+  );
 };
 
 export default SignUp;
