eb66f60a7da4b9c75d26795e4bd48a35eeb4fa6a
Roberto Guerra <prosaka@hotmail.com>
2021-02-08 22:23:49 -0300

    input & button

---
+++ aulas/07-gobarber-mobile/src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { forModalPresentationIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
+import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProps {
+  children: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
