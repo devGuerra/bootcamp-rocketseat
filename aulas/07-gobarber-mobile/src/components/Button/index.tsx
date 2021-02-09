82e6233e88ab55aa889c53362fcb4f9d59ee0bcd
Roberto Guerra <prosaka@hotmail.com>
2021-02-09 20:46:26 -0300

    integrando com unform

--- aulas/07-gobarber-mobile/src/components/Button/index.tsx
+++ aulas/07-gobarber-mobile/src/components/Button/index.tsx
@@ -1,4 +1,3 @@
-import { forModalPresentationIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 import React from 'react';
 import { RectButtonProps } from 'react-native-gesture-handler';
 
